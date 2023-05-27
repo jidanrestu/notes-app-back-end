559b00b7bda8f442a4caf6020799acd8d0391519
Jidan Restu Kurniawan <jidanrk47@gmail.com>
2023-05-27 13:49:51 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable eol-last */
+/* eslint-disable quotes */
+/* eslint-disable linebreak-style */
+const notes = [];
+
+module.exports = notes;
